81087555c2ee2f6dbfbf28cfdb3cd17c440ba4db
Jeremias Rauwolf <jeremias.rauwolf123@gmail.com>
2021-07-14 13:26:36 +0200

    database

---
+++ Sweet-Fast-DL/User.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[userID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [vorname] VARCHAR(50) NOT NULL, 
+    [zuname] VARCHAR(50) NOT NULL, 
+    [passwort] VARCHAR(50) NOT NULL, 
+    [email] VARCHAR(50) NOT NULL, 
+    [telefonnummer] VARCHAR(50) NOT NULL, 
+    [strasse] VARCHAR(50) NOT NULL, 
+    [hausnummer] INT NOT NULL, 
+    [türnummer] INT NULL, 
+    [plz] INT NOT NULL, 
+    [ort] NVARCHAR(50) NOT NULL 
+)
