5def174f067eada17bcc077bc045f57fa3284825
Jeremias Rauwolf <jeremias.rauwolf123@gmail.com>
2021-07-14 16:56:52 +0200

    relativer pfad

--- Sweet-Fast-BL/Class1.cs
+++ Sweet-Fast-BL/Class1.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-
+using System.IO;
 namespace Sweet_Fast_BL
 {
     public class Class1
     {
         internal static SqlConnection getConnection()
         {
-            string conSTring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jerem\source\repos\Zitate\DL_Zitate\Zitate.mdf;Integrated Security=True;Connect Timeout=5";
-            SqlConnection conn = new SqlConnection(conSTring);
+            List<string> dirs = new List<string>(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory).Split('\\'));
+            dirs.RemoveAt(dirs.Count - 1); //letztes Verzeichnis entfernen
+            string conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + String.Join(@"\", dirs) + @"\DL_Zitate\Zitate.mdf;Integrated Security=True;Connect Timeout=5";
+            SqlConnection conn = new SqlConnection(conString);
             conn.Open(); // sollte mit try catch gesichert werden
             return conn;
         }
