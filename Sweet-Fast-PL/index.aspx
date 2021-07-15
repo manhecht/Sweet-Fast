ee7a6b1dbc812e8f9f73fdb7e101b4abb97e39f5
sevstef <sevstef@yahoo.de>
2021-07-15 11:15:27 +0200

    Login Seite (Index.aspx) mit Inhalt - keine Logik

--- Sweet-Fast-PL/index.aspx
+++ Sweet-Fast-PL/index.aspx
@@ -5,11 +5,38 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+    <title>Sweet & Fast</title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+        </div>
+        <div id="divLogin">
+            <asp:Label ID="lblLogin" runat="server" Text="Ich bin schon registriert und will was süßes😋 "></asp:Label>
+            <br />
+            <asp:TextBox ID="txtEmailLogin" runat="server" placeholder="E-Mail"></asp:TextBox>
+            <asp:TextBox ID="txtPasswortLogin" runat="server" placeholder="Passwort"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnLogin" runat="server" Text="Login und gleich bestellen" />
+        </div>
+        <div id="divRegister">
+            <asp:Label ID="lblRegister" runat="server" Text="Gib mir einen Account Daddy🍆 "></asp:Label>
+            <br />
+            <asp:TextBox ID="txtVornameRegister" runat="server" placeholder="Vorname"></asp:TextBox>
+            <asp:TextBox ID="txtZunameRegister" runat="server" placeholder="Zuname"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtStreetRegister" runat="server" placeholder="Straße"></asp:TextBox>
+            <asp:TextBox ID="txtHausnummerRegister" runat="server" placeholder="Hausnummer"></asp:TextBox>
+            <asp:TextBox ID="txtTürnummerRegister" runat="server" placeholder="Türnummer"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtPLZRegister" runat="server" placeholder="PLZ"></asp:TextBox>
+            <asp:TextBox ID="txtOrtRegister" runat="server" placeholder="Ort"></asp:TextBox>
+            <asp:TextBox ID="txtTelefonRegister" runat="server" placeholder="Telefon"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtEmailRegister" runat="server" placeholder="E-Mail"></asp:TextBox>
+            <asp:TextBox ID="txtPasswortRegister" runat="server" placeholder="Passwort"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnRegister" runat="server" Text="Registrieren" />
         </div>
     </form>
 </body>
--- Sweet-Fast-PL/index.aspx.designer.cs
+++ Sweet-Fast-PL/index.aspx.designer.cs
@@ -22,5 +22,149 @@ namespace Sweet_Fast_PL
         /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// lblLogin-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLogin;
+
+        /// <summary>
+        /// txtEmailLogin-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmailLogin;
+
+        /// <summary>
+        /// txtPasswortLogin-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPasswortLogin;
+
+        /// <summary>
+        /// btnLogin-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogin;
+
+        /// <summary>
+        /// lblRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblRegister;
+
+        /// <summary>
+        /// txtVornameRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtVornameRegister;
+
+        /// <summary>
+        /// txtZunameRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtZunameRegister;
+
+        /// <summary>
+        /// txtStreetRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtStreetRegister;
+
+        /// <summary>
+        /// txtHausnummerRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtHausnummerRegister;
+
+        /// <summary>
+        /// txtTürnummerRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTürnummerRegister;
+
+        /// <summary>
+        /// txtPLZRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPLZRegister;
+
+        /// <summary>
+        /// txtOrtRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtOrtRegister;
+
+        /// <summary>
+        /// txtTelefonRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelefonRegister;
+
+        /// <summary>
+        /// txtEmailRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmailRegister;
+
+        /// <summary>
+        /// txtPasswortRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPasswortRegister;
+
+        /// <summary>
+        /// btnRegister-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegister;
     }
 }
