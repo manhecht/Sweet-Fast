8076a40d23bda48b443542ae3b4a93e911d9fe83
Manuel Hecht <manuelhecht8@gmail.com>
2021-07-15 13:19:57 +0200

    User Klasse

--- Sweet-Fast-BL/User.cs
+++ Sweet-Fast-BL/User.cs
@@ -8,6 +8,73 @@ namespace Sweet_Fast_BL
 {
     public class User
     {
+        private int userId;
+        private String vorname;
+        private String zuname;
+        private String passwort;
+        private String email;
+        private String strasse;
+        private int hausnummer;
+        private int türnummer;
+        private int plz;
+        private String ort;
+        private int telefonnummer;
 
+
+        public int UserId
+        {
+            get { return userId; }
+            internal set { userId = value; }
+        }
+        public string Vorname
+        {
+            get { return vorname; }
+            internal set { vorname = value; }
+        }
+        public string Zuname
+        {
+            get { return zuname; }
+            internal set { zuname = value; }
+        }
+        public string Passwort
+        {
+            get { return passwort; }
+            internal set { passwort = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            internal set { email = value; }
+        }
+        public int Telefonnummer
+        {
+            get { return telefonnummer; }
+            internal set { telefonnummer = value; }
+        }
+        public String Strasse
+        {
+            get { return strasse; }
+            internal set { strasse = value; }
+        }
+        public int Hausnummer
+        {
+            get { return hausnummer; }
+            internal set { hausnummer = value; }
+        }
+        public int Türnummer
+        {
+            get { return türnummer; }
+            internal set { türnummer = value; }
+        }
+        public int Plz
+        {
+            get { return plz; }
+            internal set { plz = value; }
+        }
+        public string Ort
+        {
+            get { return ort; }
+            internal set { ort = value; }
+        }
     }
 }
